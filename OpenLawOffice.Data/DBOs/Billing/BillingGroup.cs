03f5023a00296a05c6ca278f54ecbc1173a94550
lucasnodine <lucasnodine@gmail.com>
2015-02-14 16:43:11 -0600

    Work towards billing.

---
+++ OpenLawOffice.Data/DBOs/Billing/BillingGroup.cs
@@ -0,0 +1,130 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="BillingGroup.cs" company="Nodine Legal, LLC">
+// Licensed to Nodine Legal, LLC under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  Nodine Legal, LLC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace OpenLawOffice.Data.DBOs.Billing
+{
+    using System;
+    using AutoMapper;
+
+    [Common.Models.MapMe]
+    public class BillingGroup : Core
+    {
+        [ColumnMapping(Name = "id")]
+        public int Id { get; set; }
+
+        [ColumnMapping(Name = "title")]
+        public string Title { get; set; }
+
+        [ColumnMapping(Name = "lastrun")]
+        public DateTime? LastRun { get; set; }
+
+        [ColumnMapping(Name = "nextrun")]
+        public DateTime? NextRun { get; set; }
+
+        [ColumnMapping(Name = "amount")]
+        public decimal Amount { get; set; }
+
+        public void BuildMappings()
+        {
+            Dapper.SqlMapper.SetTypeMap(typeof(BillingGroup), new ColumnAttributeTypeMapper<BillingGroup>());
+            Mapper.CreateMap<DBOs.Billing.BillingGroup, Common.Models.Billing.BillingGroup>()
+                .ForMember(dst => dst.IsStub, opt => opt.UseValue(false))
+                .ForMember(dst => dst.Created, opt => opt.ResolveUsing(db =>
+                {
+                    return db.UtcCreated.ToSystemTime();
+                }))
+                .ForMember(dst => dst.Modified, opt => opt.ResolveUsing(db =>
+                {
+                    return db.UtcModified.ToSystemTime();
+                }))
+                .ForMember(dst => dst.Disabled, opt => opt.ResolveUsing(db =>
+                {
+                    return db.UtcDisabled.ToSystemTime();
+                }))
+                .ForMember(dst => dst.CreatedBy, opt => opt.ResolveUsing(db =>
+                {
+                    return new Common.Models.Account.Users()
+                    {
+                        PId = db.CreatedByUserPId,
+                        IsStub = true
+                    };
+                }))
+                .ForMember(dst => dst.ModifiedBy, opt => opt.ResolveUsing(db =>
+                {
+                    return new Common.Models.Account.Users()
+                    {
+                        PId = db.ModifiedByUserPId,
+                        IsStub = true
+                    };
+                }))
+                .ForMember(dst => dst.DisabledBy, opt => opt.ResolveUsing(db =>
+                {
+                    if (!db.DisabledByUserPId.HasValue) return null;
+                    return new Common.Models.Account.Users()
+                    {
+                        PId = db.DisabledByUserPId.Value,
+                        IsStub = true
+                    };
+                }))
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.Title, opt => opt.MapFrom(src => src.Title))
+                .ForMember(dst => dst.LastRun, opt => opt.MapFrom(src => src.LastRun))
+                .ForMember(dst => dst.NextRun, opt => opt.MapFrom(src => src.NextRun))
+                .ForMember(dst => dst.Amount, opt => opt.MapFrom(src => src.Amount));
+
+            Mapper.CreateMap<Common.Models.Billing.BillingGroup, DBOs.Billing.BillingGroup>()
+                .ForMember(dst => dst.UtcCreated, opt => opt.ResolveUsing(db =>
+                {
+                    return db.Created.ToDbTime();
+                }))
+                .ForMember(dst => dst.UtcModified, opt => opt.ResolveUsing(db =>
+                {
+                    return db.Modified.ToDbTime();
+                }))
+                .ForMember(dst => dst.UtcDisabled, opt => opt.ResolveUsing(db =>
+                {
+                    return db.Disabled.ToDbTime();
+                }))
+                .ForMember(dst => dst.CreatedByUserPId, opt => opt.ResolveUsing(model =>
+                {
+                    if (model.CreatedBy == null || !model.CreatedBy.PId.HasValue)
+                        return Guid.Empty;
+                    return model.CreatedBy.PId.Value;
+                }))
+                .ForMember(dst => dst.ModifiedByUserPId, opt => opt.ResolveUsing(model =>
+                {
+                    if (model.ModifiedBy == null || !model.ModifiedBy.PId.HasValue)
+                        return Guid.Empty;
+                    return model.ModifiedBy.PId.Value;
+                }))
+                .ForMember(dst => dst.DisabledByUserPId, opt => opt.ResolveUsing(model =>
+                {
+                    if (model.DisabledBy == null) return null;
+                    return model.DisabledBy.PId;
+                }))
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.Title, opt => opt.MapFrom(src => src.Title))
+                .ForMember(dst => dst.LastRun, opt => opt.MapFrom(src => src.LastRun))
+                .ForMember(dst => dst.NextRun, opt => opt.MapFrom(src => src.NextRun))
+                .ForMember(dst => dst.Amount, opt => opt.MapFrom(src => src.Amount));
+        }
+    }
+}
---
+++ OpenLawOffice.Data/DBOs/Billing/BillingGroupMatter.cs
@@ -0,0 +1,144 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="BillingGroupMatter.cs" company="Nodine Legal, LLC">
+// Licensed to Nodine Legal, LLC under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  Nodine Legal, LLC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace OpenLawOffice.Data.DBOs.Billing
+{
+    using System;
+    using AutoMapper;
+
+    [Common.Models.MapMe]
+    public class BillingGroupMatter : Core
+    {
+        [ColumnMapping(Name = "id")]
+        public Guid Id { get; set; }
+
+        [ColumnMapping(Name = "matter_id")]
+        public Guid MatterId { get; set; }
+
+        [ColumnMapping(Name = "billing_group_id")]
+        public int BillingGroupId { get; set; }
+
+        public void BuildMappings()
+        {
+            Dapper.SqlMapper.SetTypeMap(typeof(BillingGroupMatter), new ColumnAttributeTypeMapper<BillingGroupMatter>());
+            Mapper.CreateMap<DBOs.Billing.BillingGroupMatter, Common.Models.Billing.BillingGroupMatter>()
+                .ForMember(dst => dst.IsStub, opt => opt.UseValue(false))
+                .ForMember(dst => dst.Created, opt => opt.ResolveUsing(db =>
+                {
+                    return db.UtcCreated.ToSystemTime();
+                }))
+                .ForMember(dst => dst.Modified, opt => opt.ResolveUsing(db =>
+                {
+                    return db.UtcModified.ToSystemTime();
+                }))
+                .ForMember(dst => dst.Disabled, opt => opt.ResolveUsing(db =>
+                {
+                    return db.UtcDisabled.ToSystemTime();
+                }))
+                .ForMember(dst => dst.CreatedBy, opt => opt.ResolveUsing(db =>
+                {
+                    return new Common.Models.Account.Users()
+                    {
+                        PId = db.CreatedByUserPId,
+                        IsStub = true
+                    };
+                }))
+                .ForMember(dst => dst.ModifiedBy, opt => opt.ResolveUsing(db =>
+                {
+                    return new Common.Models.Account.Users()
+                    {
+                        PId = db.ModifiedByUserPId,
+                        IsStub = true
+                    };
+                }))
+                .ForMember(dst => dst.DisabledBy, opt => opt.ResolveUsing(db =>
+                {
+                    if (!db.DisabledByUserPId.HasValue) return null;
+                    return new Common.Models.Account.Users()
+                    {
+                        PId = db.DisabledByUserPId.Value,
+                        IsStub = true
+                    };
+                }))
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.Matter, opt => opt.ResolveUsing(db =>
+                {
+                    return new Common.Models.Matters.Matter()
+                    {
+                        Id = db.MatterId,
+                        IsStub = true
+                    };
+                }))
+                .ForMember(dst => dst.BillingGroup, opt => opt.ResolveUsing(db =>
+                {
+                    return new Common.Models.Billing.BillingGroup()
+                    {
+                        Id = db.BillingGroupId,
+                        IsStub = true
+                    };
+                }));
+
+            Mapper.CreateMap<Common.Models.Billing.ExpenseMatter, DBOs.Billing.ExpenseMatter>()
+                .ForMember(dst => dst.UtcCreated, opt => opt.ResolveUsing(db =>
+                {
+                    return db.Created.ToDbTime();
+                }))
+                .ForMember(dst => dst.UtcModified, opt => opt.ResolveUsing(db =>
+                {
+                    return db.Modified.ToDbTime();
+                }))
+                .ForMember(dst => dst.UtcDisabled, opt => opt.ResolveUsing(db =>
+                {
+                    return db.Disabled.ToDbTime();
+                }))
+                .ForMember(dst => dst.CreatedByUserPId, opt => opt.ResolveUsing(model =>
+                {
+                    if (model.CreatedBy == null || !model.CreatedBy.PId.HasValue)
+                        return Guid.Empty;
+                    return model.CreatedBy.PId.Value;
+                }))
+                .ForMember(dst => dst.ModifiedByUserPId, opt => opt.ResolveUsing(model =>
+                {
+                    if (model.ModifiedBy == null || !model.ModifiedBy.PId.HasValue)
+                        return Guid.Empty;
+                    return model.ModifiedBy.PId.Value;
+                }))
+                .ForMember(dst => dst.DisabledByUserPId, opt => opt.ResolveUsing(model =>
+                {
+                    if (model.DisabledBy == null) return null;
+                    return model.DisabledBy.PId;
+                }))
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.MatterId, opt => opt.ResolveUsing(model =>
+                {
+                    if (model.Matter == null || !model.Matter.Id.HasValue)
+                        return Guid.Empty;
+                    return model.Matter.Id.Value;
+                }))
+                .ForMember(dst => dst.BillingGroupId, opt => opt.ResolveUsing(model =>
+                {
+                    if (model.BillingGroup == null || !model.BillingGroup.Id.HasValue)
+                        return Guid.Empty;
+                    return model.BillingGroup.Id.Value;
+                }));
+        }
+    }
+}
